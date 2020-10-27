224c973deb90b809597f17705251903ed6171067
Colton O'Connor <colton.shawn.oconnor@gmail.com>
2020-10-27 00:31:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,13 +27,13 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
   index = 0
-  result = ""
+  result_index = 0
+  result = []
   while planeteer_calls.length > index do 
     valid_calls.each do |element| 
       if element == planeteer_calls[index]
-        result = element
-      else
-        result = nil
+        result[result_index] = element
+        result_index += 1
       end
     end
     index += 1
